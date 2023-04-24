ab1bbd08a0ea845f14baabb63a759dfcc6fac4e1
Sidney Pang <sidneypang@hotmail.co.uk>
2023-04-24 16:10:31 -0400

    added hero section, finished

---
+++ src/components/BackgroundCircles.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import { motion } from "framer-motion";
+
+type Props = {};
+
+function BackgroundCircles({}: Props) {
+  return (
+    <motion.div
+      className="relative flex justify-center items-center"
+      initial={{
+        opacity: 0,
+      }}
+      animate={{
+        scale: [1, 2, 2, 3, 1],
+        opacity: [0.1, 0.2, 0.4, 0.8, 0.1, 1.0],
+        borderRadius: ["20%", "20%", "50%", "80%", "20%"],
+      }}
+      transition={{
+        duration: 2.5,
+      }}
+    >
+      <div className="absolute border border-[#F7AB0A] opacity-20 rounded-full w-[650px] h-[650px] mt-52 animate-pulse" />
+    </motion.div>
+  );
+}
+
+export default BackgroundCircles;
