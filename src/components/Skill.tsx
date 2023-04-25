e2be4271d8d283e6e8a9eaf1cf782a9ecb38fbd1
Sidney Pang <sidneypang@hotmail.co.uk>
2023-04-25 12:44:05 -0400

    added skills, animated

--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -4,9 +4,11 @@ import { motion } from "framer-motion";
 
 type Props = {
   directionLeft?: boolean;
+  image?: string;
+  progression?: string;
 };
 
-function Skill({ directionLeft }: Props) {
+function Skill({ directionLeft, image, progression }: Props) {
   return (
     <div className="group relative flex cursor-pointer">
       <motion.img
@@ -16,12 +18,12 @@ function Skill({ directionLeft }: Props) {
         }}
         whileInView={{ opacity: 1, x: 0 }}
         transition={{ duration: 1 }}
-        src="https://www.citypng.com/public/uploads/preview/js-javascript-round-logo-icon-png-11662226392lsrrajcm0y.png"
+        src={`${image}`}
         className="rounded-full border border-gray-500 object-cover w-24 h-24 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out"
       />
       <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0">
         <div className="flex items-center justify-center h-full">
-          <p className="text-3xl font-bold text-black opacity-100">100%</p>
+          <p className="text-3xl font-bold text-black opacity-100">{progression}</p>
         </div>
       </div>
     </div>
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -7,6 +7,100 @@ import Skill from "./Skill";
 type Props = {};
 
 function Skills({}: Props) {
+  const skills = [
+    {
+      name: "JavaScript",
+      image:
+        "https://www.citypng.com/public/uploads/preview/js-javascript-round-logo-icon-png-11662226392lsrrajcm0y.png",
+      progression: "70%",
+    },
+    {
+      name: "React",
+      image: "https://logowik.com/content/uploads/images/react.jpg",
+      progression: "70%",
+    },
+    {
+      name: "HTML",
+      image:
+        "https://w7.pngwing.com/pngs/201/90/png-transparent-logo-html-html5.png",
+      progression: "90%",
+    },
+    {
+      name: "CSS",
+      image:
+        "https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582747_1280.png",
+      progression: "50%",
+    },
+    {
+      name: "NodeJS",
+      image:
+        "https://seeklogo.com/images/N/nodejs-logo-FBE122E377-seeklogo.com.png",
+      progression: "60%",
+    },
+    {
+      name: "Typescript",
+      image:
+        "https://www.typescripttutorial.net/wp-content/uploads/2020/04/favicon.png",
+      progression: "50%",
+    },
+    {
+      name: "PostgreSQL",
+      image: "https://logowik.com/content/uploads/images/postgresql6815.jpg",
+      progression: "75%",
+    },
+    {
+      name: "Material UI",
+      image: "https://mui.com/static/logo.png",
+      progression: "80%",
+    },
+    {
+      name: "Tailwind CSS",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Tailwind_CSS_Logo.svg/1024px-Tailwind_CSS_Logo.svg.png",
+      progression: "60%",
+    },
+    {
+      name: "Bootstrap",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/1280px-Bootstrap_logo.svg.png",
+      progression: "60%",
+    },
+    {
+      name: "NextJS",
+      image:
+        "https://d2nir1j4sou8ez.cloudfront.net/wp-content/uploads/2021/12/nextjs-boilerplate-logo.png",
+      progression: "60%",
+    },
+    {
+      name: "ThreeJS",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Three.js_Icon.svg/1200px-Three.js_Icon.svg.png",
+      progression: "60%",
+    },
+    {
+      name: "MongoDB",
+      image:
+        "https://pbs.twimg.com/profile_images/1452637606559326217/GFz_P-5e_400x400.png",
+      progression: "40%",
+    },
+    {
+      name: "Blender",
+      image:
+        "https://w7.pngwing.com/pngs/414/62/png-transparent-blender-hd-logo-thumbnail.png",
+      progression: "45%",
+    },
+    {
+      name: "Vite",
+      image: "https://vitejs.dev/logo-with-shadow.png",
+      progression: "65%",
+    },
+    {
+      name: "Git",
+      image: "https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png",
+      progression: "65%",
+    },
+  ];
+
   return (
     <motion.div
       className=" flex relative flex-col text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center"
@@ -23,16 +117,22 @@ function Skills({}: Props) {
       </h3>
 
       <div className="grid grid-cols-4 gap-5 ">
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
+        {skills?.slice(0, skills.length / 2).map((skill) => (
+          <Skill
+            key={skill.name}
+            image={skill.image}
+            progression={skill.progression}
+          />
+        ))}
+
+        {skills?.slice(skills.length / 2, skills.length).map((skill) => (
+          <Skill
+            key={skill.name}
+            image={skill.image}
+            progression={skill.progression}
+            directionLeft
+          />
+        ))}
       </div>
     </motion.div>
   );
