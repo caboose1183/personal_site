5eee1d055ded00b553f12961376bc5ba3817369a
Sidney Pang <sidneypang@hotmail.co.uk>
2023-04-25 13:08:53 -0400

    fixed head error

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <title>caboose1183</title>
         <link rel="shortcut icon" href="/static/favicon.ico" />
       </Head>
       <body>
